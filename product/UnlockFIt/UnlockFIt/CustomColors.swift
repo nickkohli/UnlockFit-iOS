d709a31b4797174bd539a8f9f85525ec698f3670
zlac300 <zlac300@live.rhul.ac.uk>
2024-12-06 14:29:51 +0000

    Updated CustomColors to use Hex colours.

--- product/UnlockFIt/UnlockFIt/CustomColors.swift
+++ product/UnlockFIt/UnlockFIt/CustomColors.swift
@@ -8,32 +8,48 @@
 
 import SwiftUI
 
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        let scanner = Scanner(string: hex)
+        if hex.hasPrefix("#") {
+            scanner.currentIndex = hex.index(after: hex.startIndex)
+        }
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+        let red = Double((rgbValue & 0xFF0000) >> 16) / 255.0
+        let green = Double((rgbValue & 0x00FF00) >> 8) / 255.0
+        let blue = Double(rgbValue & 0x0000FF) / 255.0
+        self.init(red: red, green: green, blue: blue)
+    }
+}
+
 struct CustomColors {
     // Fitness Rings - Primary
-    static let ringRed = Color(red: 1.0, green: 0.26, blue: 0.33) // Hex: #FF4355
-    static let ringGreen = Color(red: 0.4, green: 0.85, blue: 0.4) // Hex: #66D966
-    static let ringBlue = Color(red: 0.3, green: 0.63, blue: 1.0) // Hex: #4FA0FF
+    static let ringRed = Color(hex: "#FF4355")
+    static let ringGreen = Color(hex: "#66D966")
+    static let ringBlue = Color(hex: "#4FA0FF")
 
     // Fitness Rings - Secondary (Much Darker)
-    static let ringRed2 = Color(red: 0.4, green: 0.0, blue: 0.1) // Hex: #660019
-    static let ringGreen2 = Color(red: 0.05, green: 0.2, blue: 0.05) // Hex: #0D330D
-    static let ringBlue2 = Color(red: 0.0, green: 0.15, blue: 0.4) // Hex: #002666
+    static let ringRed2 = Color(hex: "#660019")
+    static let ringGreen2 = Color(hex: "#0D330D")
+    static let ringBlue2 = Color(hex: "#002666")
 
     // Nike Run Club Neon
-    static let neonYellow = Color(red: 0.89, green: 1.0, blue: 0.22) // Hex: #E3FF38
-    static let neonYellow2 = Color(red: 0.4, green: 0.45, blue: 0.1) // Hex: #66731A
+    static let neonYellow = Color(hex: "#E3FF38")
+    static let neonYellow2 = Color(hex: "#66731A")
 
     // Badges - Primary
-    static let bronze = Color(red: 0.8, green: 0.5, blue: 0.2) // Hex: #CC8040
-    static let silver = Color(red: 0.75, green: 0.75, blue: 0.75) // Hex: #BFBFBF
-    static let gold = Color(red: 1.0, green: 0.84, blue: 0.0) // Hex: #FFD700
-    static let platinum = Color(red: 0.9, green: 0.9, blue: 0.93) // Hex: #E6E6ED
+    static let bronze = Color(hex: "#CC8040")
+    static let silver = Color(hex: "#BFBFBF")
+    static let gold = Color(hex: "#FFD700")
+    static let platinum = Color(hex: "#E6E6ED")
 
     // Badges - Secondary (Much Darker)
-    static let bronze2 = Color(red: 0.4, green: 0.2, blue: 0.0) // Hex: #663300
-    static let silver2 = Color(red: 0.3, green: 0.3, blue: 0.3) // Hex: #4D4D4D
-    static let gold2 = Color(red: 0.5, green: 0.35, blue: 0.0) // Hex: #805900
-    static let platinum2 = Color(red: 0.5, green: 0.5, blue: 0.55) // Hex: #808089
+    static let bronze2 = Color(hex: "#663300")
+    static let silver2 = Color(hex: "#4D4D4D")
+    static let gold2 = Color(hex: "#805900")
+    static let platinum2 = Color(hex: "#808089")
     
     static let lightGray = Color(white: 0.6)
 }

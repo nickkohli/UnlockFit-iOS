019f8b0f2774887fcd4532a9ebe509004d557804
zlac300 <zlac300@live.rhul.ac.uk>
2025-03-25 22:51:23 +0000

    Updated default move goals in AppState

--- product/UnlockFIt/UnlockFIt/AppState.swift
+++ product/UnlockFIt/UnlockFIt/AppState.swift
@@ -2,4 +2,7 @@ import SwiftUI
 
 class AppState: ObservableObject {
     @Published var isLoggedIn: Bool = false // Tracks whether the user is logged in
+    @Published var stepsGoal: Int = 10000 // Default step goal, user-adjustable
+    @Published var calorieGoal: Int = 500 // Default calorie goal, user-adjustable
+    @Published var minutesGoal: Int = 30 // Default active minutes goal, user-adjustable
 }
